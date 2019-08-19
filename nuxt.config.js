6422293bcfca434b6b3fcaec2eaf04bc61579130
Raymond Cheng <akulubala@gmail.com>
2019-08-19 22:03:06 +0800

    add demo for nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,61 +1,58 @@
-
 module.exports = {
   mode: 'universal',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [{ src: '~/plugins/vue-product-zoomer.js', mode: 'client' }],
   /*
-  ** Nuxt.js dev-modules
-  */
-  devModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  devModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://github.com/nuxt-community/modules/tree/master/packages/bulma
     '@nuxtjs/bulma',
   ],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     postcss: {
       preset: {
         features: {
-          customProperties: false
-        }
-      }
+          customProperties: false,
+        },
+      },
     },
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
-  }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,171 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-product-zoomer-demo
-      </h1>
-      <h2 class="subtitle">
-        My fabulous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <ProductZoomer :base-images="images" :base-zoomer-options="zoomerOptions"></ProductZoomer>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      images: {
+        thumbs: [
+          {
+            id: 1,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/1.jpeg"
+          },
+          {
+            id: 2,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/2.jpeg"
+          },
+          {
+            id: 3,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/3.jpeg"
+          },
+          {
+            id: 4,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/4.jpeg"
+          },
+          {
+            id: 5,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/5.jpeg"
+          },
+          {
+            id: 6,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/6.jpeg"
+          },
+          {
+            id: 7,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/7.jpeg"
+          },
+          {
+            id: 8,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/8.jpeg"
+          },
+          {
+            id: 9,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/thumbs/9.jpeg"
+          }
+        ],
+        normal_size: [
+          {
+            id: 1,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/1.jpeg"
+          },
+          {
+            id: 2,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/2.jpeg"
+          },
+          {
+            id: 3,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/3.jpeg"
+          },
+          {
+            id: 4,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/4.jpeg"
+          },
+          {
+            id: 5,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/5.jpeg"
+          },
+          {
+            id: 6,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/6.jpeg"
+          },
+          {
+            id: 7,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/7.jpeg"
+          },
+          {
+            id: 8,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/8.jpeg"
+          },
+          {
+            id: 9,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/normal_size/9.jpeg"
+          }
+        ],
+        large_size: [
+          {
+            id: 1,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/1.jpeg"
+          },
+          {
+            id: 2,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/2.jpeg"
+          },
+          {
+            id: 3,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/3.jpeg"
+          },
+          {
+            id: 4,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/4.jpeg"
+          },
+          {
+            id: 5,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/5.jpeg"
+          },
+          {
+            id: 6,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/6.jpeg"
+          },
+          {
+            id: 7,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/7.jpeg"
+          },
+          {
+            id: 8,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/8.jpeg"
+          },
+          {
+            id: 9,
+            url:
+              "https://yoohooworld.com/assets/images/vue_product_zoomer/large_size/9.jpeg"
+          }
+        ]
+      },
+      zoomerOptions: {
+        zoomFactor: 3,
+        pane: "pane",
+        hoverDelay: 300,
+        namespace: "zoomer-left",
+        move_by_click: false,
+        scroll_items: 4,
+        choosed_thumb_border_color: "#dd2c00",
+        scroller_position: "left"
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -49,8 +179,8 @@ export default {
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
